853482bbaafe5c082b5553d42521c93f6b77f10a
ferranclosa <ferranclosa@gmail.com>
2020-11-25 13:08:46 +0100

    Assus Crisis

---
+++ src/main/java/com/closa/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.closa.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
