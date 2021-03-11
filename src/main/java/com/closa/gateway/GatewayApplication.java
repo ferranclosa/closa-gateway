a65903da6e44234337e4c360b83c556a2c1e14b7
Ferran Closa Cervera <36318284+ferranclosa@users.noreply.github.com>
2021-03-11 11:15:46 +0100

    Initial Commit Added Gateway functionality Added OAuth2 Resource-server functionality Added OAuth2 recognizer functionality

--- src/main/java/com/closa/gateway/GatewayApplication.java
+++ src/main/java/com/closa/gateway/GatewayApplication.java
@@ -2,15 +2,39 @@ package com.closa.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+
 public class GatewayApplication {
 
+	@CrossOrigin(origins = {"http://localhost:3000"})
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	/*@Bean
+	CorsConfigurationSource corsConfigurationSource(){
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("*"));
+		config.setAllowedMethods(Arrays.asList("OPTIONS", "GET", "POST"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "X-Requested-Width", "X-PINGOTHER", "Content-Type", "Authorization"));
+		config.applyPermitDefaultValues();
+		config.setAllowCredentials(true);
+		source.registerCorsConfiguration("/auth/**", config);
+		source.registerCorsConfiguration("/api/**", config);
+		return source;
+	}*/
 }
